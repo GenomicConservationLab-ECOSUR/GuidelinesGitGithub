6218a88976e0781004f4081b77a600b938201d6c
GGConservationLab-ECOSUR <163353267+GGConservationLab-ECOSUR@users.noreply.github.com>
2024-12-06 17:17:24 -0600

    Update Git_QuickGuide.sh

--- Git_QuickGuide.sh
+++ Git_QuickGuide.sh
@@ -1,31 +1,36 @@
-# Seguir las instrucciones:
-
-# 1. Crear una nueva rama
-$ git branch newRLadies
-# 2. Entrar a la rama que necesitas
-$ git checkup newRLadies
-# 3. Añadir cambios
-$ add file
+#################################################################################################
+A continuación se muestra un ejemplo de como clonar y sincronizar repositorios.
+Advertencia: Nunca trabajes directamente en la rama principal (origin main)!
+#################################################################################################
+# 1: Clona el repositorio
+$ git clone https://github.com/GenomicConservationLab-ECOSUR/Repositorio
+# 2: Actualiza el repositorio local (en tu computadora) para obtener los últimos cambios.
+$ git pull origin main
+# 3: Crear una nueva rama
+$ git branch Branch_01
+# 4: Entrar a la rama que acabas de crear
+$ git checkup Branch_01
+# 3: Crea y edita tus archivos, scripts, etc desde tu computadora. Para agregar los nuesvos cambios de un archivo:
+$ add script.sh
 # 4. Hacer commit
-$ git commit --messagge"add Rscript"
+$ git commit --messagge"add script de bash"
 # 5. Hacer un git push 
-$ git push --set-upstream origin newRLadies
+$ git push --set-upstream origin Branch_01
 # 6. Revisar los cambios en github
+#################################################################################################
+Si deseas crear nuevas versiones de tu código o scripts
+# 1: Crea una segunda rama y cambiate a ella.
+$ git checkout -b Branch_02
+# 2. Empuja la nueva rama al repositorio de github
+ git push --set-upstream Branch_02
+# 3. Genera tu código de prueba y depositalo en la nueva rama:
+$ git add NewScript.sh
+# 4. Hacer commit
+$ git commit --messagge"new version script control"
+# 5. Hacer un git push 
+$ git push --set-upstream Branch_02
+# Si ddeseas fusiona las dos ramas, primero regresa a la rama base Branch_01
+$ git checkup Branch_01
+# 8. Fusionar Branch_01 Branch_02
+$ git merge Branch_02
 
-# 7. Cambiarte a la rama "issue-1" (que necesitas):
-$ git checkout issue-1
-# 8. Fusionar  issue-1 con la rama que deseas
-$ git merge newRLadies
-
-
-# Step 1: Clone the repository or update your local repository with the latest changes.
-git pull origin main
-
-# Step 2: Switch to the base branch of the pull request.
-git checkout main
-
-# Step 3: Merge the head branch into the base branch.
-git merge newRLadies
-
-# Step 4: Push the changes.
-git push -u origin main
